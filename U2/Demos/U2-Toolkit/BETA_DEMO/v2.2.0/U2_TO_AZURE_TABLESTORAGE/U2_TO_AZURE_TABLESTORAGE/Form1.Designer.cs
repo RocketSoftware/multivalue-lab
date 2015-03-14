a34015374ef40f85fcd80d88b5146c56b1ed9aa7
rajank <rkumar@rs.com>
2015-03-13 22:39:05 -0600

    Added U2 Toolkit for .NET BETA SSIS/CLOUD DEMO Source Code
    Added U2 Toolkit for .NET BETA SSIS/CLOUD DEMO Source Code

---
+++ U2/Demos/U2-Toolkit/BETA_DEMO/v2.2.0/U2_TO_AZURE_TABLESTORAGE/U2_TO_AZURE_TABLESTORAGE/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace U2_TO_AZURE_TABLESTORAGE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(470, 357);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(187, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "U2_TO_AZURE_TABLESTORAGE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 381);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ U2/Demos/U2-Toolkit/BETA_DEMO/v2.2.0/U2_TO_AZURE_TABLESTORAGE/U2_TO_AZURE_TABLESTORAGE/Form1.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using U2.Data.Client;
+
+namespace U2_TO_AZURE_TABLESTORAGE
+{
+    public partial class Form1 : Form
+    {
+        string accountName = "mykey";
+
+        string accountKey = "mypass";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.textBox1.AppendText("Start..." + Environment.NewLine);
+
+                //strat get U2 data
+
+                U2ConnectionStringBuilder conn_bldr = new U2ConnectionStringBuilder();
+                conn_bldr.UserID = "admin";
+                conn_bldr.Password = "**";
+                conn_bldr.Server = "192.168.102.132";
+                conn_bldr.ServerType = "universe";
+                conn_bldr.Database = "HS.SALES";
+                conn_bldr.AccessMode = "Native";
+                conn_bldr.RpcServiceType = "uvcs";
+                string lConnStr = conn_bldr.ConnectionString;
+                U2Connection lConn = new U2Connection();
+                lConn.ConnectionString = lConnStr;
+                lConn.Open();
+                Console.WriteLine("Connected...");
+                U2Command cmd = lConn.CreateCommand();
+
+                // CUSTID,FNAME,LNAME : Single Value
+                //PRODID, BUY_DATE    : Multi Value
+                //Syntax : Action=?;File=?;Attributes=?;Where=?;Sort
+
+                cmd.CommandText = string.Format("Action=Select;File=CUSTOMER;Attributes=CUSTID,FNAME,LNAME,PRODID,BUY_DATE;Where=CUSTID>0;Sort=CUSTID");
+
+                U2DataAdapter da = new U2DataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                DataTable dt = ds.Tables[0];
+
+                //end get U2 data
+
+                //load table storage
+                string lTableStorageName = "CUSTOMER3";
+                StorageCredentials creds = new StorageCredentials(accountName, accountKey);
+                CloudStorageAccount account = new CloudStorageAccount(creds, useHttps: true);
+                CloudTableClient client = account.CreateCloudTableClient();
+                CloudTable table = client.GetTableReference("CUSTOMER6");
+                table.CreateIfNotExists();
+                string lUri = table.Uri.ToString();
+                //  Console.WriteLine(table.Uri.ToString());
+                this.textBox1.AppendText(lUri + Environment.NewLine);
+                foreach (DataRow item in dt.Rows)
+                {
+                    var t1 = item["CUSTID"].ToString();
+                    var t2 = item["Z_MV_KEY"].ToString();
+                    DateTime lhiredate;
+                    if (item["BUY_DATE"] == DBNull.Value)
+                    {
+                        lhiredate = DateTime.Now;// new DateTime();
+                    }
+                    else
+                    {
+                        lhiredate = (DateTime)item["BUY_DATE"];
+                    }
+                    CustomerEntity entity = new CustomerEntity(t1,t2)
+                    {
+                        FNAME = (string)item["FNAME"],
+                        LNAME = (string)item["LNAME"],
+                        PRODID = (string)item["PRODID"],
+                        BUY_DATE = lhiredate
+                    };
+                    TableOperation insertOperation = TableOperation.InsertOrReplace(entity);
+                    table.Execute(insertOperation);
+                    this.textBox1.AppendText("Entity inserted!" + Environment.NewLine);
+                }
+                this.textBox1.AppendText("End..." + Environment.NewLine);
+            }
+
+            catch (Exception ex)
+            {
+
+                this.textBox1.AppendText(ex.Message + Environment.NewLine);
+
+            }
+
+        }
+    }
+}
